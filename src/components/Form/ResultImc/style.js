841195e2465a8cae388eaf8c614af93f80df750f
William Silva <williamsilva20062005@gmail.com>
2024-03-06 08:49:15 -0300

    style: Estilizado text das inforamções de resultado do Imc e o Valor do Imc

---
+++ src/components/Form/ResultImc/style.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  resultImc: {
+    marginTop: 15,
+    paddingTop: 60,
+    borderRadius: 50,
+    alignItems: "center",
+    width: "100%",
+  },
+  numberImc: {
+    color: "#FF0043",
+    fontSize: 48,
+    fontWeight: "bold",
+  },
+  information: {
+    fontSize: 18,
+    color: "#FF0043",
+    fontWeight: "bold",
+  },
+});
+
+export default styles;
---
+++ src/components/Title/style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  boxTitle: {
+    alignItems: "center",
+    justifyContent: "center",
+
+    padding: 10,
+  },
+  textTitle: {
+    color: "#FF0043",
+    fontSize: 24,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+});
+
+export default styles;
